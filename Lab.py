665bae05965b7e92a317005ec3980604769d0e6b
Mohsen <Mohsen@gmail.com>
2026-02-18 13:51:51 +0300

    Solve the Lab

---
+++ Lab.py
@@ -0,0 +1,13 @@
+some_number = range(45,211)
+for number in some_number:
+    if number == 100:
+        continue
+    if number == 206:
+        break
+    print(number)
+
+user = "What is the profuct of 7 * 24? "
+while input(user) != "168":
+    print("Your Answer is wrong try again..")
+else:
+    print("You answered this Question correctly")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print("You answered this Question correctly")
